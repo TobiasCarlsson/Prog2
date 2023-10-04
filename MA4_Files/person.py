960d3421b1ef24a229e05a9df054a93c2e7bcf2a
Tobias Carlsson <tobias.carlzzon@gmail.com>
2023-10-04 13:49:41 +0200

    Fix

--- MA4_Files/person.py
+++ MA4_Files/person.py
@@ -6,17 +6,29 @@ class Person(object):
 	def __init__(self, age):
 		lib.Person_new.argtypes = [ctypes.c_int]
 		lib.Person_new.restype = ctypes.c_void_p
-		lib.Person_get.argtypes = [ctypes.c_void_p]
-		lib.Person_get.restype = ctypes.c_int
-		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+		lib.Person_getAge.argtypes = [ctypes.c_void_p]
+		lib.Person_getAge.restype = ctypes.c_int
+		lib.Person_setAge.argtypes = [ctypes.c_void_p,ctypes.c_int]
+		lib.Person_getDecades.argtypes = [ctypes.c_void_p]
+		lib.Person_getDecades.restype = ctypes.c_double
+
+		lib.Person_fib.argtypes = [ctypes.c_void_p]
+		lib.Person_fib.restype = ctypes.c_int
+
 		lib.Person_delete.argtypes = [ctypes.c_void_p]
 		self.obj = lib.Person_new(age)
 
-	def get(self):
-		return lib.Person_get(self.obj)
+	def getAge(self):
+		return lib.Person_getAge(self.obj)
 
-	def set(self, age):
-		lib.Person_set(self.obj, age)
+	def setAge(self, age):
+		lib.Person_setAge(self.obj, age)
+
+	def getDecades(self):
+		return lib.Person_getDecades(self.obj)
+	
+	def fib(self):
+		return lib.Person_fib(self.obj)
         
 	def __del__(self):
 		return lib.Person_delete(self.obj)
